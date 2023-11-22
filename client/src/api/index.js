90e4548448f924c53cf417149727e6a4beeddf1e
Muhammad Dujana Aziz <79629886+Dujana-aziz@users.noreply.github.com>
2023-11-22 06:50:13 +0500

    modified responsiveness of logout button

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
 const API = axios.create({ baseURL: 'https://locations-nx7n.onrender.com' })
+// https://locations-nx7n.onrender.com
+// http://localhost:5000
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
